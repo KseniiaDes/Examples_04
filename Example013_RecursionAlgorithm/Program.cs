4cab0b6fe05eac3994552ee8d908e9743a8d1ec1
Kseniia Des <kseniya250496@mail.ru>
2022-12-08 04:02:48 +0300

    Add Fibonacci

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -44,16 +44,32 @@
 // FillArray(matrix);
 // PrintArray(matrix);
 
-double Factorial(int n)
+// double Factorial(int n)
+// {
+//     //1! = 1
+//     //0! = 1
+//     if (n == 1) return 1;
+//     else return n * Factorial(n-1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! {Factorial(i)}");
+// }
+
+// // Console.WriteLine(Factorial(5));
+
+
+//f(1) = 1
+//f(2) = 1
+//f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
 {
-    //1! = 1
-    //0! = 1
-    if (n == 1) return 1;
-    else return n * Factorial(n-1);
-}
-for (int i = 1; i < 40; i++)
-{
-    Console.WriteLine($"{i}! {Factorial(i)}");
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
-// Console.WriteLine(Factorial(5));
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) {Fibonacci(i)}");
+}
